96a3f8204f0cf030e3cd86b202d9e19829313fab
mrzcookie <91168951+mrzcookie@users.noreply.github.com>
2025-06-18 23:02:15 -0500

    Implement ender chest override functionality

--- src/main/java/com/artillexstudios/axvaults/listeners/PlayerInteractListener.java
+++ src/main/java/com/artillexstudios/axvaults/listeners/PlayerInteractListener.java
@@ -1,8 +1,10 @@
 package com.artillexstudios.axvaults.listeners;
 
 import com.artillexstudios.axvaults.commands.PlayerCommand;
+import com.artillexstudios.axvaults.guis.VaultSelector;
 import com.artillexstudios.axvaults.placed.PlacedVaults;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
@@ -10,6 +12,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.jetbrains.annotations.NotNull;
+
+import static com.artillexstudios.axvaults.AxVaults.CONFIG;
 
 public class PlayerInteractListener implements Listener {
 
@@ -19,15 +23,23 @@ public class PlayerInteractListener implements Listener {
         if (event.getClickedBlock() == null) return;
 
         final Player player = event.getPlayer();
+        if (event.getClickedBlock().getType() == Material.ENDER_CHEST && CONFIG.getBoolean("override-ender-chests")) {
+            event.setCancelled(true);
+            event.setUseInteractedBlock(Event.Result.DENY);
+            event.setUseItemInHand(Event.Result.DENY);
+
+            new VaultSelector().open(player);
+            return;
+        }
+
         final Location location = event.getClickedBlock().getLocation();
+        if (PlacedVaults.getVaults().containsKey(location)) {
+            event.setCancelled(true);
+            event.setUseInteractedBlock(Event.Result.DENY);
+            event.setUseItemInHand(Event.Result.DENY);
 
-        if (!PlacedVaults.getVaults().containsKey(location)) return;
-
-        event.setCancelled(true);
-        event.setUseInteractedBlock(Event.Result.DENY);
-        event.setUseItemInHand(Event.Result.DENY);
-
-        final Integer vault = PlacedVaults.getVaults().get(location);
-        new PlayerCommand().open(player, vault, true);
+            final Integer vault = PlacedVaults.getVaults().get(location);
+            new PlayerCommand().open(player, vault, true);
+        }
     }
 }
