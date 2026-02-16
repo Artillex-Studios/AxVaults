989da888f9eb68d8c07fad56a4e07da546be077b
snakehugo <35538588+snakehugo@users.noreply.github.com>
2026-02-16 21:00:16 +0100

    Fix EnderVaults converter class resolution across server mappings

--- src/main/java/com/artillexstudios/axvaults/converters/EnderVaultsConverter.java
+++ src/main/java/com/artillexstudios/axvaults/converters/EnderVaultsConverter.java
@@ -15,8 +15,10 @@ import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.File;
 import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Base64;
 import java.util.UUID;
 
@@ -26,20 +28,42 @@ public class EnderVaultsConverter {
     private Class<?> nbtItemStackClass;
     private Class<?> nbtTagCompoundClass;
     private Class<?> nbtReadLimiterClass;
-    final String version = "1.20.1";
 
     private Method readNbt;
+    private Method nbtTagListSizeMethod;
+    private Method nbtTagListGetMethod;
+    private Method nbtTagCompoundIsEmptyMethod;
+    private Method nbtItemStackCreateMethod;
+    private Method asBukkitCopy;
+    private Constructor<?> nbtReadLimiterConstructor;
 
     public boolean init() {
         Class<?> nbtToolsClass;
+        String cbVersion = Bukkit.getServer().getClass().getPackage().getName();
+        cbVersion = cbVersion.substring(cbVersion.lastIndexOf('.') + 1);
+
         try {
+            nbtToolsClass = findClass(
+                    "net.minecraft.nbt.NbtIo",
+                    "net.minecraft.server." + cbVersion + ".NBTCompressedStreamTools"
+            );
 
-            nbtToolsClass = Class.forName("net.minecraft.server." + version + ".NBTCompressedStreamTools");
-
-            nbtReadLimiterClass = Class.forName("net.minecraft.server." + version + ".NBTReadLimiter");
-            nbtTagListClass = Class.forName("net.minecraft.server." + version + ".NBTTagList");
-            nbtItemStackClass = Class.forName("net.minecraft.server." + version + ".ItemStack");
-            nbtTagCompoundClass = Class.forName("net.minecraft.server." + version + ".NBTTagCompound");
+            nbtReadLimiterClass = findClass(
+                    "net.minecraft.nbt.NbtAccounter",
+                    "net.minecraft.server." + cbVersion + ".NBTReadLimiter"
+            );
+            nbtTagListClass = findClass(
+                    "net.minecraft.nbt.ListTag",
+                    "net.minecraft.server." + cbVersion + ".NBTTagList"
+            );
+            nbtItemStackClass = findClass(
+                    "net.minecraft.world.item.ItemStack",
+                    "net.minecraft.server." + cbVersion + ".ItemStack"
+            );
+            nbtTagCompoundClass = findClass(
+                    "net.minecraft.nbt.CompoundTag",
+                    "net.minecraft.server." + cbVersion + ".NBTTagCompound"
+            );
 
         } catch (ClassNotFoundException e) {
             LogUtils.error("&#FF0000[AxVaults] EnderVaults converter: Unable to find classes needed for NBT. Are you sure we support this Minecraft version?", e);
@@ -47,10 +71,41 @@ public class EnderVaultsConverter {
         }
 
         try {
-            readNbt = nbtToolsClass.getDeclaredMethod("a", DataInput.class, Integer.TYPE, nbtReadLimiterClass);
+            readNbt = findMethod(nbtToolsClass,
+                    new String[]{"a", "readUnnamedTag", "read"},
+                    DataInput.class,
+                    Integer.TYPE,
+                    nbtReadLimiterClass
+            );
+            if (readNbt == null) {
+                readNbt = findMethod(nbtToolsClass,
+                        new String[]{"a", "readUnnamedTag", "read"},
+                        DataInput.class,
+                        nbtReadLimiterClass
+                );
+            }
+            if (readNbt == null) throw new NoSuchMethodException("Unable to find matching read method");
             readNbt.setAccessible(true);
+
+            nbtTagListSizeMethod = findMethod(nbtTagListClass, new String[]{"size"});
+            nbtTagListGetMethod = findMethod(nbtTagListClass, new String[]{"get", "k", "getCompound"}, int.class);
+            nbtTagCompoundIsEmptyMethod = findMethod(nbtTagCompoundClass, new String[]{"isEmpty", "g"});
+            if (nbtTagListSizeMethod == null || nbtTagListGetMethod == null || nbtTagCompoundIsEmptyMethod == null) {
+                throw new NoSuchMethodException("Unable to find required NBT list/compound methods");
+            }
+            nbtItemStackCreateMethod = findStaticMethodWithParam(nbtItemStackClass, nbtItemStackClass, nbtTagCompoundClass);
+            nbtReadLimiterConstructor = nbtReadLimiterClass.getConstructor(long.class);
+
+            Class<?> craftItemStackClass = findClass(
+                    "org.bukkit.craftbukkit." + cbVersion + ".inventory.CraftItemStack",
+                    "org.bukkit.craftbukkit.inventory.CraftItemStack"
+            );
+            asBukkitCopy = craftItemStackClass.getMethod("asBukkitCopy", nbtItemStackClass);
         } catch (NoSuchMethodException e) {
             LogUtils.error("&#FF0000[AxVaults] EnderVaults converter: Unable to find writeNbt or readNbt method. Are you sure we support this Minecraft version?", e);
+            return false;
+        } catch (ClassNotFoundException e) {
+            LogUtils.error("&#FF0000[AxVaults] EnderVaults converter: Unable to find CraftItemStack class. Are you sure we support this Minecraft version?", e);
             return false;
         }
 
@@ -134,16 +189,16 @@ public class EnderVaultsConverter {
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(Base64.getDecoder().decode(encoded));
         DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
 
-        Object nbtReadLimiter = nbtReadLimiterClass.getConstructor(long.class).newInstance(Long.MAX_VALUE);
-        Object readInvoke = readNbt.invoke(null, dataInputStream, 0, nbtReadLimiter);
+        Object nbtReadLimiter = nbtReadLimiterConstructor.newInstance(Long.MAX_VALUE);
+        Object readInvoke;
+        if (readNbt.getParameterCount() == 3) {
+            readInvoke = readNbt.invoke(null, dataInputStream, 0, nbtReadLimiter);
+        } else {
+            readInvoke = readNbt.invoke(null, dataInputStream, nbtReadLimiter);
+        }
 
         Object nbtTagList = nbtTagListClass.cast(readInvoke);
-        Method nbtTagListSizeMethod = nbtTagListClass.getMethod("size");
-        Method nbtTagListGetMethod = nbtTagListClass.getMethod("get", int.class);
         int nbtTagListSize = (int) nbtTagListSizeMethod.invoke(nbtTagList);
-
-        Method nbtTagCompoundIsEmptyMethod = nbtTagCompoundClass.getMethod("isEmpty");
-        Method nbtItemStackCreateMethod = nbtItemStackClass.getMethod("createStack", nbtTagCompoundClass);
         Object items = Array.newInstance(nbtItemStackClass, nbtTagListSize);
 
         for (int i = 0; i < nbtTagListSize; ++i) {
@@ -180,8 +235,56 @@ public class EnderVaultsConverter {
     }
 
     private ItemStack toBukkitItem(Object itemStack) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-
-        Method asBukkitCopy = Class.forName("org.bukkit.craftbukkit." + version + ".inventory.CraftItemStack").getMethod("asBukkitCopy", nbtItemStackClass);
         return (ItemStack) asBukkitCopy.invoke(null, itemStack);
+    }
+
+    private Class<?> findClass(String... classNames) throws ClassNotFoundException {
+        for (String className : classNames) {
+            try {
+                return Class.forName(className);
+            } catch (ClassNotFoundException ignored) {
+            }
+        }
+        throw new ClassNotFoundException("None of the classes were found: " + String.join(", ", classNames));
+    }
+
+    private Method findMethod(Class<?> owner, String[] names, Class<?>... parameterTypes) {
+        for (String name : names) {
+            try {
+                Method method = owner.getMethod(name, parameterTypes);
+                method.setAccessible(true);
+                return method;
+            } catch (NoSuchMethodException ignored) {
+            }
+
+            try {
+                Method method = owner.getDeclaredMethod(name, parameterTypes);
+                method.setAccessible(true);
+                return method;
+            } catch (NoSuchMethodException ignored) {
+            }
+        }
+        return null;
+    }
+
+    private Method findStaticMethodWithParam(Class<?> owner, Class<?> returnType, Class<?> parameterType) throws NoSuchMethodException {
+        for (Method method : owner.getDeclaredMethods()) {
+            if (!Modifier.isStatic(method.getModifiers())) continue;
+            if (method.getParameterCount() != 1) continue;
+            if (!parameterType.isAssignableFrom(method.getParameterTypes()[0])) continue;
+            if (!returnType.isAssignableFrom(method.getReturnType())) continue;
+            method.setAccessible(true);
+            return method;
+        }
+
+        for (Method method : owner.getMethods()) {
+            if (!Modifier.isStatic(method.getModifiers())) continue;
+            if (method.getParameterCount() != 1) continue;
+            if (!parameterType.isAssignableFrom(method.getParameterTypes()[0])) continue;
+            if (!returnType.isAssignableFrom(method.getReturnType())) continue;
+            method.setAccessible(true);
+            return method;
+        }
+        throw new NoSuchMethodException("Unable to find static method on " + owner.getName() + " with param " + parameterType.getName());
     }
 }
